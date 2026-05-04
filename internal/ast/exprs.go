e21e9497da09065b4eac9a9de0c7a28fe49246f6
shotowon <sanzhar.maratov@proton.me>
2026-05-04 17:18:53 +0500

    AST: exprs definition

---
+++ internal/ast/exprs.go
@@ -0,0 +1,88 @@
+package ast
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/shotowon/kica/internal/gears"
+	"github.com/shotowon/kica/internal/tokens"
+)
+
+type AtomExpr struct {
+	Value *tokens.Token
+}
+
+func (e *AtomExpr) exprNode() {}
+func (e *AtomExpr) Location() gears.Location {
+	return e.Value.Location
+}
+
+func (e *AtomExpr) String() string {
+	if e.Value == nil {
+		return "<nil>"
+	}
+	return e.Value.Literal
+}
+
+type UnaryExpr struct {
+	Value Expr
+	Op    *tokens.Token
+}
+
+func (e *UnaryExpr) exprNode() {}
+func (e *UnaryExpr) Location() gears.Location {
+	loc := e.Op.Location
+	loc.End = e.Value.Location().End
+	return loc
+}
+
+func (e *UnaryExpr) String() string {
+	op := "?"
+	if e.Op != nil {
+		op = e.Op.Literal
+	}
+	return fmt.Sprintf("(%s %v)", op, e.Value)
+}
+
+type BinaryExpr struct {
+	LHS Expr
+	RHS Expr
+	Op  *tokens.Token
+}
+
+func (e *BinaryExpr) exprNode() {}
+func (e *BinaryExpr) Location() gears.Location {
+	loc := e.LHS.Location()
+	loc.End = e.RHS.Location().End
+	return loc
+}
+
+func (e *BinaryExpr) String() string {
+	op := "?"
+	if e.Op != nil {
+		op = e.Op.Literal
+	}
+	return fmt.Sprintf("(%s %v %v)", op, e.LHS, e.RHS)
+}
+
+type FunctionCallExpr struct {
+	Name *tokens.Token
+	Args []Expr
+	Loc  gears.Location
+}
+
+func (e *FunctionCallExpr) exprNode() {}
+func (e *FunctionCallExpr) Location() gears.Location {
+	return e.Loc
+}
+
+func (e *FunctionCallExpr) String() string {
+	args := strings.Builder{}
+	for i, arg := range e.Args {
+		args.WriteString(arg.String())
+		if i+1 < len(e.Args) {
+			args.WriteString(", ")
+		}
+	}
+	return fmt.Sprintf("(%s(%s))", e.Name.Literal, args.String())
+}
